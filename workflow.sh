91e356cba79b73939c1110472fbe7f87d0af9326
Dániel Kántor <git@daniel-kantor.com>
2017-09-23 19:18:40 +0200

    Load zshrc before running vim

--- workflow.sh
+++ workflow.sh
@@ -25,7 +25,7 @@ run_custom_commands_of() {
 
 run_editor_in_directory_of() {
     PROJECT=$1
-    urxvt -cd "$PROJECTS/$PROJECT" -hold -e nvim "$PROJECTS/$PROJECT" > /dev/null &
+    urxvt -cd "$PROJECTS/$PROJECT" -hold -e zsh -c 'source ~/.zshrc; nvim "$PROJECTS/$PROJECT"' > /dev/null &
 }
 
 open_terminal_in_directory_of() {
